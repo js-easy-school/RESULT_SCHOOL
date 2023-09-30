36810e8fb4c1ebcf610692c904884722354681ef
js-easy-school <supergeroy11@gmail.com>
2023-09-30 17:41:34 +0300

    Update Task_2 (HOC) <- Added second to way

--- hooks/start-hooks/src/app/components/exercises/hoc/simpleComponent.jsx
+++ hooks/start-hooks/src/app/components/exercises/hoc/simpleComponent.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const SimpleComponent = ({ onLogin, onLogOut, isAuth }) => {
+const SimpleComponent = ({ onLogin, onLogOut }) => {
+    const [isAuth, setIsAuth] = useState(false);
+
     const handleLogin = () => {
+        setIsAuth(true);
         onLogin();
     };
 
     const handleLogOut = () => {
+        setIsAuth(false);
         onLogOut();
     };
 
     return (
         <div>
             {isAuth ? (
-                <button className="btn btn-danger" onClick={handleLogOut}>Выйти из системы</button>
+                <button className="btn btn-danger" onClick={handleLogOut}>
+                    Выйти из системы
+                </button>
             ) : (
-                <button className="btn btn-primary" onClick={handleLogin}>Войти</button>
+                <button className="btn btn-primary" onClick={handleLogin}>
+                    Войти
+                </button>
             )}
         </div>
     );
@@ -23,8 +31,38 @@ const SimpleComponent = ({ onLogin, onLogOut, isAuth }) => {
 
 SimpleComponent.propTypes = {
     onLogin: PropTypes.func.isRequired,
-    onLogOut: PropTypes.func.isRequired,
-    isAuth: PropTypes.bool.isRequired
+    onLogOut: PropTypes.func.isRequired
 };
 
 export default SimpleComponent;
+
+// import React from "react";
+// import PropTypes from "prop-types";
+//
+// const SimpleComponent = ({ onLogin, onLogOut, isAuth }) => {
+//     const handleLogin = () => {
+//         onLogin();
+//     };
+//
+//     const handleLogOut = () => {
+//         onLogOut();
+//     };
+//
+//     return (
+//         <div>
+//             {isAuth ? (
+//                 <button className="btn btn-danger" onClick={handleLogOut}>Выйти из системы</button>
+//             ) : (
+//                 <button className="btn btn-primary" onClick={handleLogin}>Войти</button>
+//             )}
+//         </div>
+//     );
+// };
+//
+// SimpleComponent.propTypes = {
+//     onLogin: PropTypes.func.isRequired,
+//     onLogOut: PropTypes.func.isRequired,
+//     isAuth: PropTypes.bool.isRequired
+// };
+//
+// export default SimpleComponent;
