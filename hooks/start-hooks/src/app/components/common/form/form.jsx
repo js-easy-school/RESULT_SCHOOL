ed97d80926fc3121eea6de482b2eb6953c24ab85
js-easy-school <supergeroy11@gmail.com>
2023-11-12 18:08:38 +0300

    ok

---
+++ hooks/start-hooks/src/app/components/common/form/form.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { validator } from "../../../utils/validator";
+
+const FormComponent = ({ children }) => {
+    const [data, setData] = useState({});
+    const [errors, setErrors] = useState({});
+    useEffect(() => { validate(); }, [data]);
+    const handleChange = (target) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+    const validate = () => {
+        const errors = validator(data, validatorConfig);
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+    const clonedElements = React.Children
+    return <form>{children}</form>;
+};
+
+FormComponent.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+};
+
+export default FormComponent;
